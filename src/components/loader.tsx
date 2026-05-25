53fc4c6866b4eb9b1c8320a9367e822d6684d123
matanmalka1 <matan1391@gmail.com>
2026-05-25 19:02:25 +0300

    commit

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -118,9 +118,10 @@ const AppLoader = ({
     return (
       <div
         className={clsx(
-          "pointer-events-none fixed left-1/2 top-1/2 z-40 -translate-x-1/2 -translate-y-1/2",
+          "fixed inset-0 z-40 grid place-items-center",
           className,
         )}
+        style={{ bottom: "var(--bottom-nav-h, 0px)" }}
         aria-live="polite"
       >
         <div className="group relative flex flex-col items-center gap-5">
